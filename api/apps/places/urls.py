87a4b53e261fb793d6133e9219d077ae35140382
miriam-moztap <ada_hack16@hackwomen.dev>
2023-04-03 22:40:11 -0600

    se modifican get para obtenerdiferentes vistas

--- api/apps/places/urls.py
+++ api/apps/places/urls.py
@@ -1,8 +1,9 @@
 from django.urls import URLPattern, path
-from .views import PlaceAPIView, PlaceAPIUpdateDeleteView
+from .views import PlaceAPIView, PlaceAPIUpdateDeleteView, PlaceGetAPIView
 
 
 urlpatterns = [
-    path('', PlaceAPIView.as_view()),
+    path('', PlaceAPIView.as_view()),    
+    path('<int:id>/', PlaceGetAPIView.as_view()),
     path('<int:id>/', PlaceAPIUpdateDeleteView.as_view()),
 ]
