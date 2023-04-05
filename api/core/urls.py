a114a8f137a7895e2a00fe9e244016f7f15a7fa2
silvaanis8 <ada_hack30@hackwomen.dev>
2023-04-04 22:39:29 -0400

    Creación de app comments

--- api/core/urls.py
+++ api/core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.initial.urls')),
     path('apps.places/', include('apps.places.urls')),
+    path('apps.comments/', include('apps.comments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
