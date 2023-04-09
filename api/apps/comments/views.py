f04f02958466703ad8792eb7d79da18f5493c029
SoreM <100636808+Sorele1987@users.noreply.github.com>
2023-04-09 10:52:13 -0400

    Se esta realizando la modificacion en models de comments, migraciones

--- api/apps/comments/views.py
+++ api/apps/comments/views.py
@@ -2,14 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Comment
-from.serializers import CommentSerializers
+from .serializers import CommentSerializers
 
 # Create your views here.
 
 
 class CommentView(APIView):
-     def post(self, request):
-        serializer = CommentSerializers(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+   def post(self, request):
+      serializer = CommentSerializers(data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
