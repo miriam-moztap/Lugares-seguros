ac24f834d351db4955de364e39f30daf31c4113b
SoreM <100636808+Sorele1987@users.noreply.github.com>
2023-04-09 11:05:55 -0400

    Se agrego delete y la url correspondiente para poder correrlo

--- api/apps/comments/views.py
+++ api/apps/comments/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,3 +14,12 @@ class CommentView(APIView):
       serializer.is_valid(raise_exception=True)
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class CommentSingleView(APIView):
+
+   def delete(self, request, pk):
+      place = get_object_or_404(Comment, pk=pk)
+      place.delete()
+      return Response('Comentario eliminado', status=status.HTTP_204_NO_CONTENT)
+
+
