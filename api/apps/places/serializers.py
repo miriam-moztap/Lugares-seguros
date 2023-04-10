c63d8c1aac4466bfa11e33825ccdaeeebb7c2bda
SoreM <100636808+Sorele1987@users.noreply.github.com>
2023-04-09 21:47:53 -0400

    creado serializador de lugares para estar juntos con comentarios

--- api/apps/places/serializers.py
+++ api/apps/places/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Place
+from apps.comments.models import Comment
+from apps.comments.serializers import CommentPlaceListSerializer
 
 
 class PlaceSerializers(serializers.ModelSerializer):
@@ -8,3 +10,20 @@ class PlaceSerializers(serializers.ModelSerializer):
 
         model = Place
         fields = '__all__'
+
+class PlacelistCommentSerializer(serializers.ModelSerializer):
+
+    comment = serializers.SerializerMethodField()
+
+    class Meta:
+
+        model = Place
+        fields = (
+            'id',
+            'name',
+            'comment'
+        )
+    
+    def get_comment(self, obj):
+        selected_comment = Comment.objects.filter(place__id = obj.id)
+        return CommentPlaceListSerializer(selected_comment, many=True).data
